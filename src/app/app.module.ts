f3c72c38c8c954d33f61e50d1723bd747ec59471
Matupoom Chaiwan <matupoom001@gmail.com>
2021-11-02 10:05:15 +0700

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavComponent } from './components/nav/nav.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NavComponent,
+    SideBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+  @Input() showSideBar: boolean = false
+  @Output() showSideBarChange: EventEmitter<boolean> = new EventEmitter()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public toggleSidebar(): void {
+    this.showSideBarChange.emit(!this.showSideBar)
+  }
+}
