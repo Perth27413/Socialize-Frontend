74698538bc2f973dc76b5c8a747d021b82af363e
Arnicha <arnichamichai@gmail.com>
2021-11-03 21:37:00 +0700

    slidebar by Aon

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -6,10 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.scss']
 })
 export class SideBarComponent implements OnInit {
+  menuList: {icon: string, name: string, isActive: boolean}[] = [
+    {icon: 'fas fa-home', name: 'Home', isActive: true},
+    {icon: 'fas fa-users', name: 'Follows', isActive: false},
+    {icon: 'fas fa-star', name: 'Favorites', isActive: false}
+  ]
 
   constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  selectMenu(index: number) {
+    console.log(index)
+    switch (index) {
+      case 0:
+        this.menuList[0].isActive = true;
+        this.menuList[1].isActive = false;
+        this.menuList[2].isActive = false;
+        break;
+      case 1:
+        this.menuList[0].isActive = false;
+        this.menuList[1].isActive = true;
+        this.menuList[2].isActive = false;
+        break;
+      case 2:
+        this.menuList[0].isActive = false;
+        this.menuList[1].isActive = false;
+        this.menuList[2].isActive = true;
+        break;
+      default:
+        this.menuList[0].isActive = true;
+        this.menuList[1].isActive = false;
+        this.menuList[2].isActive = false;
+        break;
+    }
   }
+
+  cardActive(index: number) {
+
+    return {
+      'card-active' : this.menuList[index].isActive,
+      'text-active' : this.menuList[index].isActive
+    }
+  }
+  
 
 }
