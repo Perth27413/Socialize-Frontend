6aeaf8a83df045d8acdfecb529628ff18cb40f12
Purin Tadsorn <purintadsorn@gmail.com>
2021-11-07 16:25:28 +0700

    edit-scss-responsive-viewprofile

--- src/app/components/view-profile/view-profile.component.ts
+++ src/app/components/view-profile/view-profile.component.ts
@@ -6,14 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-profile.component.scss']
 })
 export class ViewProfileComponent implements OnInit {
-  numfile: number = 10
+  numfile: number = 0
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  // public countMutifile(): void {
-  //   this.numfile = $("#FileInput")[0].numfile;
+  public countMutifile(events: Event): void {
+    const target = events.target as HTMLInputElement;
+    const files = target.files as FileList;
+    this.numfile = files.length;
+    console.log(target.files);
+    // for (var arr in target.files) {
+    //   console.log(arr);
+    // }
+    // if (files) {
+    //   if (files.type != 'image/jpeg' && 'image/png') {
+    //     console.log('invalid file type must be a jpg or png');
+    //     return
+    //   }
+    //   else {
+    //     console.log('The file type is correct.');
+    //   }
+    // }
+  }
+  // uploadFile(event: any) {
+  //   const file: File = event.target.files[0];
+  //   if (file) {
+  //     if (file.type != 'image/jpeg' && 'image/png') {
+  //       console.log('invalid file type must be a jpg or png');
+  //       return
+  //     }
+  //     else {
+  //       console.log('The file type is correct.');
+  //     }
+  //   }
   // }
 }
