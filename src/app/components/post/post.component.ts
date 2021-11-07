196b9f1d3e9165d4cfcf69c7c0cff04d21f7aa61
Arnicha <arnichamichai@gmail.com>
2021-11-07 19:04:07 +0700

    laout photo by aon

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-post',
@@ -8,10 +7,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
   isShowComment: boolean = false;
+  test: string = "แมว"
+  postList: { name: string, decs: string, imageProfile: string, textContent: string, numOfPhoto: number, photoContent: { photo: string }[] }[] = [
+    { 
+      name: "Meaw Mouw",
+      decs: "UX/UI Designer",
+      imageProfile: "https://thumbs.dreamstime.com/b/funny-crazy-cat-evil-white-open-mouth-61209625.jpg",
+      textContent: "วันนี้อากาศแจ่มแจ่มแจ่มแจ่มแจ่มแจ่มแจ่ม",
+      numOfPhoto: 2,
+      photoContent: [
+        {photo: "https://www.brandbuffet.in.th/wp-content/uploads/2018/04/1-1.jpg"},
+        {photo: "https://www.brandbuffet.in.th/wp-content/uploads/2018/04/1-1.jpg"}
+      ]
+    },{ 
+      name: "Meaw Mouw",
+      decs: "UX/UI Designer",
+      imageProfile: "https://thumbs.dreamstime.com/b/funny-crazy-cat-evil-white-open-mouth-61209625.jpg",
+      textContent: "วันนี้อากาศแจ่มแจ่มแจ่มแจ่มแจ่มแจ่มแจ่ม",
+      numOfPhoto: 1,
+      photoContent: [
+        {photo: "https://www.brandbuffet.in.th/wp-content/uploads/2018/04/1-1.jpg"},
+      ]
+    },{ 
+      name: "Meaw Mouw",
+      decs: "UX/UI Designer",
+      imageProfile: "https://thumbs.dreamstime.com/b/funny-crazy-cat-evil-white-open-mouth-61209625.jpg",
+      textContent: "วันนี้อากาศแจ่มแจ่มแจ่มแจ่มแจ่มแจ่มแจ่ม",
+      numOfPhoto: 4,
+      photoContent: [
+        {photo: "https://www.brandbuffet.in.th/wp-content/uploads/2018/04/1-1.jpg"},
+        {photo: "https://www.brandbuffet.in.th/wp-content/uploads/2018/04/1-1.jpg"},
+        {photo: "https://www.brandbuffet.in.th/wp-content/uploads/2018/04/1-1.jpg"},
+        {photo: "https://www.brandbuffet.in.th/wp-content/uploads/2018/04/1-1.jpg"},
+      ]
+    },{ 
+      name: "Meaw Mouw",
+      decs: "UX/UI Designer",
+      imageProfile: "https://thumbs.dreamstime.com/b/funny-crazy-cat-evil-white-open-mouth-61209625.jpg",
+      textContent: "วันนี้อากาศแจ่มแจ่มแจ่มแจ่มแจ่มแจ่มแจ่ม",
+      numOfPhoto: 3,
+      photoContent: [
+        {photo: "https://www.brandbuffet.in.th/wp-content/uploads/2018/04/1-1.jpg"},
+        {photo: "https://www.brandbuffet.in.th/wp-content/uploads/2018/04/1-1.jpg"},
+        {photo: "https://www.brandbuffet.in.th/wp-content/uploads/2018/04/1-1.jpg"}
+      ]
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  gridPhotoLayout(index: number) {
+    return {
+      'grid1' : this.postList[index].photoContent.length === 1,
+      'grid2' : this.postList[index].photoContent.length === 2,
+      'grid3' : this.postList[index].photoContent.length === 3,
+      'photo1' : this.postList[index].photoContent.length === 1,
+      'photo2' : this.postList[index].photoContent.length === 2,
+      'photo3' : this.postList[index].photoContent.length === 3,
+      'photo-grid3' : this.postList[index].photoContent.length === 3
+    }
   }
 
   toggleShowComment() {
