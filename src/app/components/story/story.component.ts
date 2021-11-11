720eee91691e377a4747283da6b4fe49b5f259cd
Meen <meentotyou@gmail.com>
2021-11-11 21:22:47 +0700

    story layout

---
+++ src/app/components/story/story.component.ts
@@ -0,0 +1,61 @@
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
+
+@Component({
+  selector: 'app-story',
+  templateUrl: './story.component.html',
+  styleUrls: ['./story.component.scss']
+})
+export class StoryComponent implements AfterViewInit {
+
+  @ViewChild('storyBox') public scroll!: ElementRef<HTMLElement>;
+  leftScroll: boolean = false
+  rightScroll: boolean = true
+
+  constructor() {
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+
+  public async leftArrow() {
+    let maxScroll = this.scroll.nativeElement.scrollWidth - this.scroll.nativeElement.clientWidth
+    this.scroll.nativeElement.scrollLeft = this.scroll.nativeElement.scrollLeft - 160
+    await new Promise(f => setTimeout(f, 100));
+    if (this.scroll.nativeElement.scrollLeft <= 0) {
+      this.leftScroll = false
+    }
+    else if (this.scroll.nativeElement.scrollLeft < maxScroll) {
+      this.rightScroll = true
+    }
+  }
+
+  public async rightArrow() {
+    let maxScroll = this.scroll.nativeElement.scrollWidth - this.scroll.nativeElement.clientWidth
+    this.scroll.nativeElement.scrollLeft = this.scroll.nativeElement.scrollLeft + 160
+    await new Promise(f => setTimeout(f, 100));
+    if (this.scroll.nativeElement.scrollLeft >= maxScroll) {
+      this.rightScroll = false
+    }
+    else if (this.scroll.nativeElement.scrollLeft > 0) {
+      this.leftScroll = true
+    }
+  }
+
+  storyList: { user: string, image: string, isActive: boolean }[] = [
+    { user: 'https://scontent.fbkk10-1.fna.fbcdn.net/v/t1.18169-9/26238776_2101967746745773_77371830993486459_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=174925&_nc_eui2=AeH3l-9c2khCcVplkoTZRALPRz3rN7NCQ1dHPes3s0JDV23Y5OwE6f43BtUUB-id-Ihf940eWSI3nodWJE9pbYwd&_nc_ohc=ZHoBBEpQjwQAX95XbZg&tn=RFbGWcrFuZvQXM6b&_nc_ht=scontent.fbkk10-1.fna&oh=92024295d165b369568b161f30b618cf&oe=61AE1E02', image: 'https://scontent.fbkk10-1.fna.fbcdn.net/v/t1.18169-9/12219318_1656148921327660_1716789488201421922_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=8bfeb9&_nc_eui2=AeHByx7bn63Q2hnS_UBYXrcKRUaB9T8AjTJFRoH1PwCNMl28XsuWkkocBBf0iY-czD2IGFP9cWEduOOXqRDSXL51&_nc_ohc=D1hR75muk2IAX_hBTqe&_nc_ht=scontent.fbkk10-1.fna&oh=31794d499f028852d581d7f52e4bf63d&oe=61AF0946', isActive: true },
+    { user: '../../../assets/image/profile.jpg', image: '../../../assets/image/profile.jpg', isActive: true },
+    { user: '../../../assets/image/profile.jpg', image: '../../../assets/image/profile.jpg', isActive: true },
+    { user: '../../../assets/image/profile.jpg', image: '../../../assets/image/profile.jpg', isActive: true },
+    { user: '../../../assets/image/profile.jpg', image: '../../../assets/image/profile.jpg', isActive: true },
+    { user: '../../../assets/image/profile.jpg', image: '../../../assets/image/profile.jpg', isActive: true },
+    { user: '../../../assets/image/profile.jpg', image: '../../../assets/image/profile.jpg', isActive: true },
+    { user: '../../../assets/image/profile.jpg', image: '../../../assets/image/profile.jpg', isActive: true },
+    { user: '../../../assets/image/profile.jpg', image: '../../../assets/image/profile.jpg', isActive: true },
+    { user: '../../../assets/image/profile.jpg', image: '../../../assets/image/profile.jpg', isActive: true },
+    { user: '../../../assets/image/profile.jpg', image: '../../../assets/image/profile.jpg', isActive: true },
+    { user: '../../../assets/image/profile.jpg', image: '../../../assets/image/profile.jpg', isActive: true }
+  ]
+
+}
