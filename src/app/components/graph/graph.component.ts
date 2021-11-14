3260dc74f4707ee310fec82c26de458e90da2d1a
Arnicha <arnichamichai@gmail.com>
2021-11-14 11:52:31 +0700

    responsive chart

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -14,46 +14,51 @@ export class GraphComponent implements OnInit {
   ]
   basicData: any;
   dataSource: any;
-  constructor() {}
+  options: any;
+  constructor() { }
 
   ngOnInit(): void {
     this.dataSource = {
-      labels: ['facebook','google','socailize'],
+      labels: ['facebook', 'google', 'socailize'],
       datasets: [
-          {
-              data: [300, 50, 100],
-              backgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56"
-              ],
-              hoverBackgroundColor: [
-                  "#FF6384",
-                  "#36A2EB",
-                  "#FFCE56"
-              ]
-          }
-      ]
-  };
-  this.basicData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
         {
-            label: 'First Dataset',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            fill: false,
-            borderColor: '#42A5F5',
-            tension: .4
+          data: [300, 50, 100],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
+        }
+      ]
+    };
+    this.basicData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: '#42A5F5',
+          tension: .4
         },
         {
-            label: 'Second Dataset',
-            data: [28, 48, 40, 19, 86, 27, 90],
-            fill: false,
-            borderColor: '#FFA726',
-            tension: .4
+          label: 'Second Dataset',
+          data: [28, 48, 40, 19, 86, 27, 90],
+          fill: false,
+          borderColor: '#FFA726',
+          tension: .4
         }
-    ]
-};
+      ]
+    };
+    this.options = {
+      responsive: true,
+      maintainAspectRatio: false
+    };
   }
 
 
